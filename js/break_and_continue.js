708e59b9f5dfb08bfc19161025e03ba01d91f3fd
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-13 15:32:19 -0600

    Feat: While loop exercise break and continue.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,15 +1,20 @@
-var numToSkip = prompt("Can you give me an odd number between 1 and 50?");
+var userInput = prompt("Please enter an odd number between 1 and 50.");
 
-function skip(num) {
-    while(num % 2 ===0 || num > 50 || num < 1){
-        numToSkip = prompt("Try again. Please give me an odd number between 1 and 50?");
+while((parseFloat(userInput) % 2 ===0) || (parseFloat(userInput) <= 1) || (parseFloat(userInput)>= 50) || isNaN(parseFloat(userInput))){
+    alert("Try again! That was not a valid number");
+    userInput = prompt("Please enter a valid odd number between 1 and 50");
+}
+console.log("The number to skip is: " + userInput);
+console.log("");
+
+for(var i = 1; i <= 50; i++){
+    if(i % 2 === 0){
+        continue;
     }
-    for (var i = 1; i < 10; i+=2) {
-            if (i === numToSkip) {
-            console.log("Yikes you're skipping " + numToSkip);
-            continue;
-        }
-        console.log("Here is an odd number: " + i);
+    if(i == userInput){
+        console.log("Yikes! You are now skipping: " + i);
+
+    } else {
+        console.log("Here is an odd number: + " + i);
     }
 }
-skip(numToSkip)
--- js/while.js
+++ js/while.js
@@ -1,5 +1,21 @@
 var count = 1
-while(count <= 32768){
-    count *= 2
-    console.log(count)
+while(count <= 65536){
+    console.log(count);
+    count *=2
 }
+
+var allCones = Math.floor(Math.random()*50)+50;
+console.log("I have this many cones to sell: " + allCones);
+
+do{
+  var conesPurchased =  Math.floor(Math.random()*5)+1;
+  if(conesPurchased > allCones){
+      console.log("Can't sell you that many cones. You wanted " + conesPurchased + ", but I onlu have " + allCones)
+  } else {
+      allCones = allCones - conesPurchased;
+      console.log("Here are your " + conesPurchased + " cones! I have " + allCones + " left.")
+  }
+
+} while(allCones > 0)
+
+console.log("I've sold all of my cones! Time to go home!")
