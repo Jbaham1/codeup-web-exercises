c3b33edbf34c281e48efab7c3cebd2f5560741fc
Jeremiah Baham <baham.jeremiah20@gmail.com>
2020-11-17 15:58:31 -0600

    Exercise: Objects.js

---
+++ js/objects-lec.js
@@ -0,0 +1,14 @@
+"use strict";
+
+    var kamea = {
+        name: "kamea",
+        location: "San Antonio",
+        colors: ["black", "white"],
+        species: "Orca",
+        date_of_birth: Date("12/6/2013")
+    };
+
+    var tilikum = {};
+
+
+
