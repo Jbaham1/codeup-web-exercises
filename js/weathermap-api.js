e8b7141ee6daccada7474feb53db6a5c61484da7
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-06 09:55:36 -0600

    .get&.done

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     //AJAX call to openWeather
     $.get("http://api.openweathermap.org/data/2.5/weather", {
-        APPID: OPEN_WEATHER_APPID,
+        APPID: OPEN_WEATHER_APPID, //API key
         q:     "San Antonio, US"
-    });
+    }).done(function(data){
+        console.log(data);
+    })
 
 });
