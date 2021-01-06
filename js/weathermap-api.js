4873b6b1fc38e9e5e528f81725f93662195c6273
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-06 10:09:48 -0600

    icons

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //AJAX call to openWeather
     $.get("http://api.openweathermap.org/data/2.5/weather", {
         APPID: OPEN_WEATHER_APPID, //API key
-        q:     "San Antonio, US"
-    }).done(function(data){
+        q: "San Antonio, US",
+        units: "imperial"
+    }).done(function (data) {
         console.log(data);
-    })
+
+        let iconImage = '<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png"/>'
+        $("body").append(iconImage)
+    });
+
 
 });
