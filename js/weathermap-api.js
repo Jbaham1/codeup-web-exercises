b81fc6ff7e63db3ed0790b49ff0e0fbe1ae184f6
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-07 10:24:29 -0600

    City Name and date

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
     //AJAX call to openWeather
-    $.get("http://api.openweathermap.org/data/2.5/weather", {
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
         APPID: OPEN_WEATHER_APPID, //API key
         q: "San Antonio, US",
         units: "imperial"
     }).done(function (data) {
-        console.log(data);
-        let iconImage = '<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png"/>';
-        $("body").append(iconImage)
+        console.log('5 day forecast', data);
+        let iconImage = '<img src="http://openweathermap.org/img/wn/' + data.list[0].main.humidity + '@2x.png"/>';
+        let today = new Date();
+        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+        $('#city').append(" " + data.city.name + " " + data.city.country)
+            $("#forecast").append(date, +' ' )
     });
 
 
