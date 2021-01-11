dd2fdf81c63ca7c3d3494c1bbe1e03589f3ca5cb
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-11 08:15:04 -0600

    updating ajax store for dynamic review

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -1,22 +1,8 @@
 "use strict";
-mapboxgl.accessToken = mapBoxToken;
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-98.4916, 29.45252],
-    zoom: 10
-});
-
-var start = {lng: -98.4916, lat: 29.45252}
-
-$(document).ready(function () {
-    //AJAX call to openWeather
-    let weatherObj = $.get("http://api.openweathermap.org/data/2.5/forecast", {
-        APPID: OPEN_WEATHER_APPID, //API key
-        q: "San Antonio, US",
-        units: "imperial"
-    }).done(function (data) {
-        console.log(data)
-    });
+$.get("http://api.openweathermap.org/data/2.5/weather", {
+    APPID: OPEN_WEATHER_APPID,
+    q:     "San Antonio, US",
+    units: "imperial"
+}).done(function(data) {
+    console.log(data);
 });
