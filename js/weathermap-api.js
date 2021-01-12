9d84bd9d073411142ba15e2e4a939bb1b602d265
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-11 21:23:24 -0600

    card information

--- js/weathermap-api.js
+++ js/weathermap-api.js
@@ -14,11 +14,14 @@ function getWeather() {
             var utcSeconds = data.daily[i].dt;
             var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
             d.setUTCSeconds(utcSeconds);
-            var imageIcon = "<img src'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png'>"
+            // var imageIcon = "<img src'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png'>"
 
             $(divId + " .date").html(d.getMonth() + 1 + "-" + d.getDate() + "-" + d.getFullYear())
-            $(divId + " .temperature span").html(data.daily[i].temp.min + "/" + data.daily[i].temp.max)
-            $(divId + " .image").html(imageIcon)
+            $(divId + " .temperature span").html(data.daily[i].temp.min + " / " + data.daily[i].temp.max)
+            // $(divId + " .image div").html(imageIcon)
+            $(divId + " .description").html("<strong>Description: </strong>" + data.daily[i].weather[0].description)
+            $(divId + " .wind").html("<strong>Wind: </strong>" + data.daily[i].wind_speed.toFixed() + "MPH")
+            $(divId + " .pressure").html("<strong>Pressure:</strong>" + data.daily[i].pressure)
         }
     });
 }
